803aed75aee382b1c99c499ecd85b42d5021fde4
hrishabhgoell <hrishabh.w21@gmail.com>
2023-11-09 17:40:46 +0000

    Dockaer instance created

--- sample-app.sh
+++ sample-app.sh
@@ -10,9 +10,9 @@ cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
